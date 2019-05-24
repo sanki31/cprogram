62956e0d451d30d145aef4f121419e301acea313
sanki31 <50978955+sanki31@users.noreply.github.com>
2019-05-24 12:19:57 +0530

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,14 @@
-include<stdio.h>
+#include<stdio.h>
 int main()
 {
-printf("hello world\n");
-  printf("hii");
+int num1,num2,res;
+scanf("%d%d",&num1,&num2);
+res=sum(num1,num2);
+printf("sum=%d",res);
 }
+int sum(int num1,int num2)
+{
+    int num3;
+    num3=num1+num2;
+    return num3;
+}    
